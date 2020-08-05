b1740b340bb9da60a640fe359011a064c332326f
baam <baam_gg_apollo_t0326@ie8.com>
2020-08-05 17:57:33 +0800

    feat: add zh_CN

--- resources/js/Pages/Domain/Index.vue
+++ resources/js/Pages/Domain/Index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1 class="mb-8 font-bold text-3xl">域名管理</h1>
     <div class="mb-6 flex justify-between items-center">
       <search-filter v-model="form.search" class="w-full max-w-md mr-4" @reset="reset">
         <label class="block text-gray-700">Trashed:</label>

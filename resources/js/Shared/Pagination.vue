6ce86b085f6c418c65fc0814d1e3c057e469948b
baam <baam_gg_apollo_t0326@ie8.com>
2020-08-06 13:48:13 +0800

    fix: pagination style

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -1,8 +1,15 @@
 <template>
   <nav>
     <ul class="pagination">
-      <li v-for="(link, key) in links" :key="key" class="page-item">
-        <div v-if="link.url === null" :key="key" :class="{ 'disabled': link.label === 'Next'}" class="page-link">{{ link.label }}</div>
+      <li v-for="(link, key) in links" 
+        :key="key"
+        :class="{ active: link.active, disabled: link.url==null}"
+        class="page-item" >
+        <div
+          v-if="link.url === null"
+          :key="key"
+          class="page-link"
+        >{{ link.label }}</div>
         <inertia-link v-else :key="key" :href="link.url" class="page-link">{{ link.label }}</inertia-link>
       </li>
     </ul>
@@ -13,13 +20,6 @@
 export default {
   props: {
     links: Array,
-  },
-  data(){
-    return {
-      classObject:{
-
-      }
-    }
   },
   created: function () {
     console.log(this.links);

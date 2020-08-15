8ac69b448428c844e60ae92f33ac114947e54f0e
baam <youmax210139@gmail.com>
2020-08-15 18:11:50 +0800

    feat: check os

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -18,7 +18,11 @@ class PingController extends Controller
         $request->validate([
             'url' => 'required',
         ]);
-        $output = shell_exec(env('PING_PATH') . " -c4 $request->url 2>&1");
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $output = shell_exec(env('PING_PATH') . " -n4 $request->url 2>&1");
+        } else {
+            $output = shell_exec(env('PING_PATH') . " -c4 $request->url 2>&1");
+        }
         $encode = mb_detect_encoding($output, array("ASCII", 'UTF-8', "GB2312", "GBK", 'BIG5', 'CP936'));
         return mb_convert_encoding($output, 'utf-8', $encode);
 

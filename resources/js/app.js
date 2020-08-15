41ef0929e4ef70df70ea80151a8a62c721161e44
baam <youmax210139@gmail.com>
2020-08-15 17:21:30 +0800

    feat: add vue toasted and axios interceptor

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 import Popper from 'popper.js'
 import moment from 'moment'
-
-
 import '@fortawesome/fontawesome-free/css/all.css'
 
 try {
@@ -18,13 +16,28 @@ import VueMeta from 'vue-meta'
 import { InertiaApp } from '@inertiajs/inertia-vue'
 import PortalVue from 'portal-vue'
 import clipboard from 'clipboard'
+import Toasted from 'vue-toasted'
 import Axios from 'axios'
 import store from '@/Store'
 import Layout from '@/Layout/Master/Layout'
 
 Vue.config.productionTip = false
 Vue.prototype.clipboard = clipboard
+Vue.use(Toasted, {
+  position: 'top-center',
+  theme: 'toasted-primary',
+  duration: 3000
+})
 Axios.defaults.headers.common['X-CSRF-TOKEN'] = $('meta[name="csrf-token"]').attr('content')
+Axios.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  let err = {};
+  for (const [key, value] of Object.entries(error.response.data.errors)) {
+    err[key] = value.join('<br>');
+  }
+  return Promise.reject(err);
+});
 Vue.prototype.$http = Axios
 Vue.mixin({ methods: { route: window.route } })
 Vue.use(InertiaApp)

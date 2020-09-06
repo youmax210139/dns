686dc61276472cafb10fa3568c94086009dc845c
youmax <youmax210139@gmail.com>
2020-09-07 01:04:03 +0800

    feat: use daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('autowhois:run')
             ->runInBackground()
-            ->everyTenMinutes();
+            ->daily();
     }
 
     /**

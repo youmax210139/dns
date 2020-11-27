f2512f1e71073e42023720982f78853e39b41a39
youmax <youmax210139@gmail.com>
2020-11-27 10:34:22 +0800

    refactor: expand timeout

--- app/Services/Http/Http.php
+++ app/Services/Http/Http.php
@@ -27,8 +27,8 @@ class Http
         $this->client = new Client([
             'allow_redirects' => true,
             'handler' => $this->stack,
-            'timeout' => 3,
-            'connect_timeout' => 3,
+            'timeout' => 10,
+            'connect_timeout' => 10,
         ]);
     }
 
